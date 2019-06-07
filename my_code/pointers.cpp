25e39c7d25129e6bfedce9da31943eaf97e8b87a
Menghan An <anmenghan@10-18-118-105.dynapool.wireless.nyu.edu>
2019-06-07 11:47:59 -0400

    pointers 11:47

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -19,17 +19,35 @@ void inc_with_reference(int& n) {
 
 
 Thing** create_array_of_things(int n) {
+    Thing** array = new Thing * [n];
+    for(int i =0;i<= n;i++){
+        array[i] = new Thing(i);
+    }
     
-    return nullptr;
+    
+    return array;
 }
 
+
 void print_all_things(Thing** things, int n) {
+    for(int i =0;i<=n;i++){
+        cout<< things[i]->val<<" ";
+    }
+    cout <<endl;
 }
 
 void double_all_things(Thing** things, int n) {
+    for (int i = 0;i<= n;i++){
+        things[i]->val *=2;
+        
+    }
 }
 
 void delete_all_things(Thing** things, int n) {
+    for (int i =1; i<= n;i++){
+        delete things[i];
+    }
+    delete[] things;
 }
 
 
